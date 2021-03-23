34242c5fac9855c883e5fcc47047fb5f5547f6c4
DinizEduardo <eduardo.diniz987@gmail.com>
2021-03-23 11:03:27 -0300

    Construindo o array

---
+++ src/main/kotlin/br/com/alura/array/IntroArray.kt
@@ -0,0 +1,23 @@
+package br.com.alura.array
+
+fun main() {
+    val idade1 = 25
+    val idade2 = 19
+    val idade3 = 33
+
+    val maiorIdade = if (idade1 > idade2 && idade1 > idade3) {
+        idade1
+    } else if (idade2 > idade3) {
+        idade2
+    } else {
+        idade3
+    }
+
+    println(maiorIdade)
+
+    val idades = IntArray(4)
+    idades[0] = 25
+    idades[1] = 19
+    idades[2] = 33
+    idades[3] = 20
+}
